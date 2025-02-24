55734b9b4fd0819885b1c45b447c8149c01949c2
ilhamganteng1 <79833079+ilhamganteng1@users.noreply.github.com>
2025-02-24 19:28:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,22 +6,22 @@ from logging.handlers import RotatingFileHandler
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "6"))
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5839381774"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://iystampan:iystampan@iystore.yglf3jm.mongodb.net/?retryWrites=true&w=majority&appName=IYSTORE")
+DB_NAME = os.environ.get("DB_NAME", "IYSTORE")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002477743076"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002448932643"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002496261674"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002270682191"))
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "60000000000")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
@@ -30,8 +30,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "6848088376").split()):
+    ADMINS=[5196236230]
+    for x in (os.environ.get("ADMINS", "5196236230").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -62,7 +62,8 @@ USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 
 START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n <b>​🇦​​🇳​​🇩​​🇦​ ​🇭​​🇦​​🇷​​🇺​​🇸​ ​🇧​​🇪​​🇷​​🇬​​🇦​​🇧​​🇺​​🇳​​🇬​ ​🇩​​🇮​ ​🇨​​🇭​​🇦​​🇳​​🇳​​🇪​​🇱​/​🇬​​🇷​​🇺​​🇵​ ​🇸​​🇦​​🇾​​🇦​ ​🇺​​🇳​​🇹​​🇺​​🇰​ ​🇲​​🇪​​🇳​​🇴​​🇳​​🇹​​🇴​​🇳​ ​🇻​​🇮​​🇩​​🇪​​🇴​​🇳​​🇾​​🇦​ ​🇸​​🇮​​🇱​​🇦​​🇰​​🇦​​🇳​ ​🇯​​🇴​​🇮​​🇳​ ​🇰​​🇪​ ​🇨​​🇭​​🇦​​🇳​​🇳​​🇪​​🇱​ ​🇹​​🇪​​🇷​​🇱​​🇪​​🇧​​🇮​​🇭​ ​🇩​​🇦​​🇭​​🇺​​🇱​​🇺​ ​🇩​​🇮​​🇧​​🇦​​🇼​​🇦​​🇭​ ​🇮​​🇳​​🇮​
+⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇</b>")
 
 
 
@@ -97,8 +98,3 @@ def LOGGER(name: str) -> logging.Logger:
 
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
