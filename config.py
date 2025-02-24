368b690394f54feb19a964d155f306d10e1d1a29
ilhamganteng1 <79833079+ilhamganteng1@users.noreply.github.com>
2025-02-24 20:04:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ DB_URL = os.environ.get("DB_URL", "mongodb+srv://iystampan:iystampan@iystore.ygl
 DB_NAME = os.environ.get("DB_NAME", "IYSTORE")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002298018816"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002477743076"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002448932643"))
 FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002496261674"))
